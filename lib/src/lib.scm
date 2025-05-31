cb7593ea343e443cb3e7a3feb91c074e57a7d537
Alexander Lelchuk <alex0surname@gmail.com>
2025-05-31 14:33:36 +0300

    delays

--- lib/src/lib.scm
+++ lib/src/lib.scm
@@ -179,3 +179,21 @@
       (lambda (pair) `(set! ,(car pair) ,(cadr pair)))
       bindings)
     ,@exprs))
+
+
+
+
+(define (make-promise p)
+  (let ([done? #f]
+        [res #f])
+    (lambda ()
+      (if done? res
+        (begin
+          (set! done? #t)
+          (set! res (p))
+          res)))))
+
+(define-macro (delay expr)
+  `(make-promise (lambda () ,expr)))
+
+(define (force delay) (delay))
